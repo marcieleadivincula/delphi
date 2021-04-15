35140bd9bda022db77cc396477b50baca06de7c4
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-15 20:58:03 -0300

    Create unit_bairro.pas

---
+++ prEndereco/unit_bairro.pas
@@ -0,0 +1,25 @@
+unit unit_bairro;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  Tform_cadBairro = class(TForm)
+    pnlCadBairro: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  form_cadBairro: Tform_cadBairro;
+
+implementation
+
+{$R *.dfm}
+
+end.
