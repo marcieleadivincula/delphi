8fe5f31dbbc15b40ebb53ea7b649107a3f661aeb
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-15 20:58:20 -0300

    Create unit_pais.pas

---
+++ prEndereco/unit_pais.pas
@@ -0,0 +1,25 @@
+unit unit_pais;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  Tform_cadPais = class(TForm)
+    pnlcadPais: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  form_cadPais: Tform_cadPais;
+
+implementation
+
+{$R *.dfm}
+
+end.
