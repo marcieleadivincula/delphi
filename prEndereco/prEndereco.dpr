6e3ff34442fe775a7745a8548ea317b9b08c77ab
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-15 20:57:53 -0300

    Update prEndereco.dpr

--- prEndereco/prEndereco.dpr
+++ prEndereco/prEndereco.dpr
@@ -3,7 +3,16 @@ program prEndereco;
 uses
   Vcl.Forms,
   unit_endereco in 'unit_endereco.pas' {form_principal},
-  dm_dados in 'dm_dados.pas' {DataModule1: TDataModule};
+  dm_dados in 'dm_dados.pas' {DataModule1: TDataModule},
+  unit_cidade in 'unit_cidade.pas' {form_cadCidade},
+  unit_estado in 'unit_estado.pas' {form_cadEstado},
+  unit_bairro in 'unit_bairro.pas' {form_cadBairro},
+  unit_pais in 'unit_pais.pas' {form_cadPais},
+  Endereco in 'Endereco.pas',
+  Estado in 'Estado.pas',
+  Pais in 'Pais.pas',
+  Cidade in 'Cidade.pas',
+  Bairro in 'Bairro.pas';
 
 {$R *.res}
 
@@ -12,5 +21,9 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(Tform_principal, form_principal);
   Application.CreateForm(TDataModule1, DataModule1);
+  Application.CreateForm(Tform_cadCidade, form_cadCidade);
+  Application.CreateForm(Tform_cadEstado, form_cadEstado);
+  Application.CreateForm(Tform_cadBairro, form_cadBairro);
+  Application.CreateForm(Tform_cadPais, form_cadPais);
   Application.Run;
 end.
