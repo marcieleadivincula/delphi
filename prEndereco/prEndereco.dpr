63d64a4cd223f6a94db9204cb20a6129d5232d02
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-28 00:00:19 -0300

    Update prEndereco.dpr

--- prEndereco/prEndereco.dpr
+++ prEndereco/prEndereco.dpr
@@ -2,17 +2,20 @@ program prEndereco;
 
 uses
   Vcl.Forms,
-  unit_endereco in 'unit_endereco.pas' {form_principal},
-  dm_dados in 'dm_dados.pas' {DataModule1: TDataModule},
-  unit_cidade in 'unit_cidade.pas' {form_cadCidade},
-  unit_estado in 'unit_estado.pas' {form_cadEstado},
-  unit_bairro in 'unit_bairro.pas' {form_cadBairro},
-  unit_pais in 'unit_pais.pas' {form_cadPais},
-  Endereco in 'Endereco.pas',
-  Estado in 'Estado.pas',
-  Pais in 'Pais.pas',
-  Cidade in 'Cidade.pas',
-  Bairro in 'Bairro.pas';
+  Obj_Cidade in 'classes\Obj_Cidade.pas',
+  unit_CadBairro in 'unit_CadBairro.pas' {form_cadBairro},
+  unit_CadCidade in 'unit_CadCidade.pas' {form_cadCidade},
+  unit_CadEstado in 'unit_CadEstado.pas' {form_cadEstado},
+  unit_CadPais in 'unit_CadPais.pas' {form_cadPais},
+  unit_dados in 'unit_dados.pas' {dm_dados: TDataModule},
+  unit_principal in 'unit_principal.pas' {form_principal},
+  unit_CadLogradouro in 'unit_CadLogradouro.pas' {form_cadLogradouro},
+  Obj_Utilitario in 'classes\Obj_Utilitario.pas',
+  Obj_Bairro in 'classes\Obj_Bairro.pas',
+  Obj_Estado in 'classes\Obj_Estado.pas',
+  unit_Consulta in 'unit_Consulta.pas' {form_Consulta},
+  Obj_Logradouro in 'classes\Obj_Logradouro.pas',
+  Obj_Pais in 'classes\Obj_Pais.pas';
 
 {$R *.res}
 
@@ -20,10 +23,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(Tform_principal, form_principal);
-  Application.CreateForm(TDataModule1, DataModule1);
-  Application.CreateForm(Tform_cadCidade, form_cadCidade);
-  Application.CreateForm(Tform_cadEstado, form_cadEstado);
-  Application.CreateForm(Tform_cadBairro, form_cadBairro);
-  Application.CreateForm(Tform_cadPais, form_cadPais);
+  Application.CreateForm(Tdm_dados, dm_dados);
   Application.Run;
 end.
