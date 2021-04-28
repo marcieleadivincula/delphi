339f7225611f7cebe43bccedd73e9cdd0a42cdcc
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-04-28 00:00:34 -0300

    Create Obj_Utilitario.pas

---
+++ prEndereco/classes/Obj_Utilitario.pas
@@ -0,0 +1,35 @@
+unit Obj_Utilitario;
+
+interface
+
+uses
+    FMX.Forms, FMX.Edit, FMX.Memo, FMX.ListBox, FMX.Grid, System.Classes, System.SysUtils;
+
+type
+    TObj_Utilitario = class
+      public
+            class procedure LimparTela(var Form);
+    end;
+
+implementation
+
+{ TObj_Utilitario }
+
+class procedure TObj_Utilitario.LimparTela(var Form);
+Var
+   Temp: TCustomForm;
+   i:integer;
+begin
+     Temp:= TCustomForm(form);
+     for i := 0 to (Temp.ComponentCount-1) do
+        begin
+            if (Temp.Components[i] is TEdit) then
+               TEdit(Temp.Components[i]).Text := ''
+            else if (Temp.Components[i] is TMemo) then
+               TMemo(Temp.Components[i]).Text := ''
+            else if (Temp.Components[i] is TComboBox) then
+               TComboBox(Temp.Components[i]).ItemIndex := 0;
+        end;
+end;
+
+end.
